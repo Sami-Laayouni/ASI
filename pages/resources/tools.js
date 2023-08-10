eae6870923f2b78e049434a0e4eebc0965bb86b9
Sami-Laayouni <therealsamilaayouni@gmail.com>
2023-08-10 12:29:25 +0100

    tools

--- pages/resources/tools.js
+++ pages/resources/tools.js
@@ -1,7 +1,29 @@
 import style from "../../styles/Default.module.css";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 export default function Tools() {
+  const [role, setRole] = useState("all");
+  const tools = [
+    {
+      name: "Noteswap",
+      image: "/assets/images/Rescources/tools/circle.png",
+      url: "https://noteswap.org",
+      role: "all",
+    },
+    {
+      name: "Clever",
+      image: "/assets/images/Rescources/tools/clever.jpg",
+      url: "https://www.clever.com/",
+      role: "all",
+    },
+    {
+      name: "IXL",
+      image: "/assets/images/Rescources/tools/ixl.png",
+      url: "https://ixl.com/",
+      role: "students",
+    },
+  ];
   return (
     <>
       <section className={style.header}>
@@ -20,171 +42,54 @@ export default function Tools() {
         </p>
       </section>
       <section className={style.gridContainer}>
+        <section className={style.selectRole}>
+          <div
+            onClick={() => {
+              setRole("students");
+            }}
+          >
+            Students
+          </div>
+          <div
+            onClick={() => {
+              setRole("teahcers");
+            }}
+          >
+            Teachers
+          </div>
+        </section>
         <ul className={style.grid}>
-          <Link href="https://noteswap.org" target="_blank">
-            <li
-              style={{
-                width: "100%",
-                height: "100%",
-                background: "white",
-                height: "250px",
-                borderRadius: "8px",
-                boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
-                cursor: "pointer",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                flexDirection: "column",
-              }}
-            >
-              <Image
-                src="/assets/images/Rescources/tools/circle.png"
-                alt="Noteswap"
-                width={100}
-                height={110}
-              />
-              <h1 style={{ textAlign: "center" }}>Noteswap</h1>
-            </li>
-          </Link>
-
-          <li
-            style={{
-              width: "100%",
-              height: "100%",
-              background: "white",
-              height: "250px",
-              borderRadius: "8px",
-              boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
-              cursor: "pointer",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              flexDirection: "column",
-            }}
-          >
-            <Image
-              src="/assets/logo/logo.png"
-              alt="Example"
-              width={100}
-              height={110}
-            />
-            <h1 style={{ textAlign: "center" }}>Example</h1>
-          </li>
-          <li
-            style={{
-              width: "100%",
-              height: "100%",
-              background: "white",
-              height: "250px",
-              borderRadius: "8px",
-              boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
-              cursor: "pointer",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              flexDirection: "column",
-            }}
-          >
-            <Image
-              src="/assets/logo/logo.png"
-              alt="Example"
-              width={100}
-              height={110}
-            />
-            <h1 style={{ textAlign: "center" }}>Example</h1>
-          </li>
-          <li
-            style={{
-              width: "100%",
-              height: "100%",
-              background: "white",
-              height: "250px",
-              borderRadius: "8px",
-              boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
-              cursor: "pointer",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              flexDirection: "column",
-            }}
-          >
-            <Image
-              src="/assets/logo/logo.png"
-              alt="Example"
-              width={100}
-              height={110}
-            />
-            <h1 style={{ textAlign: "center" }}>Example</h1>
-          </li>
-          <li
-            style={{
-              width: "100%",
-              height: "100%",
-              background: "white",
-              height: "250px",
-              borderRadius: "8px",
-              boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
-              cursor: "pointer",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              flexDirection: "column",
-            }}
-          >
-            <Image
-              src="/assets/logo/logo.png"
-              alt="Example"
-              width={100}
-              height={110}
-            />
-            <h1 style={{ textAlign: "center" }}>Example</h1>
-          </li>
-          <li
-            style={{
-              width: "100%",
-              height: "100%",
-              background: "white",
-              height: "250px",
-              borderRadius: "8px",
-              boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
-              cursor: "pointer",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              flexDirection: "column",
-            }}
-          >
-            <Image
-              src="/assets/logo/logo.png"
-              alt="Example"
-              width={100}
-              height={110}
-            />
-            <h1 style={{ textAlign: "center" }}>Example</h1>
-          </li>
-          <li
-            style={{
-              width: "100%",
-              height: "100%",
-              background: "white",
-              height: "250px",
-              borderRadius: "8px",
-              boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
-              cursor: "pointer",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              flexDirection: "column",
-            }}
-          >
-            <Image
-              src="/assets/logo/logo.png"
-              alt="Example"
-              width={100}
-              height={110}
-            />
-            <h1 style={{ textAlign: "center" }}>Example</h1>
-          </li>
+          {tools.map(function (value) {
+            if (value.role == "all" || value.role == role || role == "all") {
+              return (
+                <Link href={value.url} target="_blank">
+                  <li
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      background: "white",
+                      height: "250px",
+                      borderRadius: "8px",
+                      boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
+                      cursor: "pointer",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      flexDirection: "column",
+                    }}
+                  >
+                    <Image
+                      src={value.image}
+                      alt={value.name}
+                      width={100}
+                      height={100}
+                    />
+                    <h1 style={{ textAlign: "center" }}>{value.name}</h1>
+                  </li>
+                </Link>
+              );
+            }
+          })}
         </ul>
       </section>
     </>
