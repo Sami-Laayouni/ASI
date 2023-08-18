50c1787e6e341ecd97a568b5b142475b144ba044
Sami-Laayouni <therealsamilaayouni@gmail.com>
2023-08-18 14:24:33 +0100

    ci: date

--- components/Events/index.js
+++ components/Events/index.js
@@ -1,20 +1,27 @@
-import { format, parseISO } from "date-fns";
+import { format, parseISO, addDays, isSameDay } from "date-fns";
 import style from "./events.module.css";
 import Link from "next/link";
 
 export default function Events({ data }) {
+  const startDate = parseISO(data.start.date);
+  const endDate = parseISO(data.end.date);
+  const nextDayDate = addDays(startDate, 1);
   return (
     <Link href={data.htmlLink}>
       <li key={data.id} className={style.container}>
         <h1>{data.summary}</h1>
-        <p className={style.detail}>
+        <p>
           {!data.start.dateTime
             ? "All day"
-            : `${format(parseISO(data.start.dateTime), "MMMM dd, yyyy - HH:mm")}
-         - 
-        ${format(parseISO(data.end.dateTime), "HH:mm")}`}
+            : `${format(parseISO(data.start.date), "HH:mm")} - 
+          ${format(parseISO(data.end.date), "HH:mm")}`}
           {" · "}
-          <span>{data.location}</span>
+          {isSameDay(startDate, endDate)
+            ? `${format(startDate, "MMMM dd, yyyy")}`
+            : isSameDay(nextDayDate, endDate)
+            ? `${format(startDate, "MMMM dd, yyyy")}`
+            : ` ${format(startDate, "MMMM dd, yyyy")} - 
+          ${format(endDate, "MMMM dd, yyyy")}`}
         </p>
 
         <p className={style.desc}>
